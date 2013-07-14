8e0d5d5ba54fb3db1b87c73114cf643e07739f65
futoase <futoase@gmail.com>
2013-07-14 14:06:38 +0900

    Modified: Tidy Web front javascript source. Move to public/app.js.

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ app.set('views', __dirname + '/views');
 app.set('view options', { layout: false });
 app.set('view cache', false);
 
+app.use(express.static(__dirname + '/public'));
 app.use(express.bodyParser());
 app.use(express.urlencoded());
 app.use(express.json());
---
+++ public/javascript/app.js
@@ -0,0 +1,61 @@
+var frontEnvironment = {
+      protocol: window.location.protocol,
+      hostname: window.location.hostname,
+      port: window.location.port,
+      origin: function () {
+        var url =  (
+          frontEnvironment.protocol + '//' + 
+          frontEnvironment.hostname
+        );
+        if (frontEnvironment.port !== "") {
+          url += ':' + frontEnvironment.port;
+        }
+        return url;
+      }
+    };
+
+var socketIoJs = {
+  addPort : (frontEnvironment.origin() + '/socket.io/socket.io.js'),
+  noPort: (frontEnvironment.origin() + '/socket.io.socket.io.js')
+};
+
+function postMessage(setting) {
+  // Set client by use of socket.io.
+  var socket = io.connect(frontEnvironment.origin(setting));
+
+  socket.on("send-message", function (data) {
+    $("#message").append(
+      (data.status.basic === true ? "is basic auth" : "no basic auth") +
+      " " +
+      data.status.method + 
+      " " + 
+      JSON.stringify(data.message)
+    );
+    $("#message").append($("<br/>"));
+  }); 
+
+  socket.on("set-basic-auth", function (data) {
+    $("#message").append("Setting basic authorization.").append($("<br/>"));
+  });
+}
+
+// Loaded socket.io.js
+$.get(socketIoJs.addPort).done(function () {
+  $.getScript(socketIoJs.addPort).done(function () {
+    postMessage({ usePort: true });
+  });
+}).fail(function() {
+  $.get(socketIoJs.noPort).done(function() {
+    $.getScript(socketIoJs.noPort).done(function () {
+      postMessage({ usePort: false });
+    });
+  }).fail(function () {
+    alert("socket.io loaded error...");
+  });
+});
+
+// Initial Setup
+$(function () {
+  $("span.host-name").text(frontEnvironment.origin());
+});
+
