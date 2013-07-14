f56417c3907bc5fb2f66b85339dad7825eef0993
futoase <futoase@gmail.com>
2013-07-15 03:14:13 +0900

    Modified: Split swig.js.

--- index.js
+++ index.js
@@ -1,37 +1,5 @@
 var EventEmitter = require('events').EventEmitter;
-var swig = require('swig');
-
-swig._cache = {};
-swig.express3 = function (path, options, fn) {
-  swig._read(path, options, function (err, str) {
-    if (err) { 
-      return fn(err); 
-    }
-    try {
-      options.filename = path;
-      var tmpl = swig.compile(str, options);
-      fn(null, tmpl(options));
-    } catch (error) {
-      fn(error);
-    }
-  });
-};
-
-swig._read = function (path, options, fn) {
-  var str = swig._cache[path];
-  if (options.cache && str && typeof str == 'string') {
-    return fn(null, str);
-  }
-  require('fs').readFile(path, 'utf8', function (err, str) {
-    if (err) {
-      return fn(err);
-    }
-    if (options.cache) {
-      swig._cache[path] = str;
-    }
-    fn(null, str);
-  });
-};
+var swig = require('./lib/swig');
 
 var express = require('express')
   , app = express()
@@ -50,11 +18,6 @@ app.use(express.bodyParser());
 app.use(express.urlencoded());
 app.use(express.json());
 app.use(express.multipart());
-
-swig.init({
-  root: __dirname + '/views',
-  allowErrors: true
-});
 
 io.configure(function() {
   io.set("transports", ["xhr-polling"]);
